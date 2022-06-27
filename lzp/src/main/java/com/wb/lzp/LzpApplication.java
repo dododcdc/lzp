25ed26f00aa582fe5c901d7340457086a2d1e7a4
dododcdc <dcdc3166@foxmail.com>
2022-06-27 11:49:15 +0800

    update

--- lzp/src/main/java/com/wb/lzp/LzpApplication.java
+++ lzp/src/main/java/com/wb/lzp/LzpApplication.java
@@ -1,13 +1,29 @@
 package com.wb.lzp;
 
+import com.wb.lzp.config.LzpHttpConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-public class LzpApplication {
+public class LzpApplication implements CommandLineRunner {
+
+	@Autowired
+	LzpHttpConfig lzpHttpConfig;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LzpApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+
+		log.info(lzpHttpConfig.test());
+
+
+	}
 }
---
+++ lzp/src/main/java/com/wb/lzp/config/LzpHttpConfig.java
@@ -0,0 +1,23 @@
+package com.wb.lzp.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+@Configuration
+public class LzpHttpConfig {
+
+
+    @Value("#{${lzp.header}}")
+    private Map<String,String> header;
+
+
+
+    public String test(){
+        return this.header.get("cookie");
+    }
+
+
+}
